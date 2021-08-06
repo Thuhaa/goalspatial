3d183331841ba987102384cac6fc222cc0f979d9
Thuhaa <josephthuha@gmail.com>
2021-08-06 21:32:32 +0300

    Commit-F

--- web/urls.py
+++ web/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-path('', views.homepage_view, name='homepage')
+path('', views.homepage_view, name='homepage'),
+path('our-projects', views.projects_view, name='our-projects'),
+path('learn', views.learn_view, name="learn"),
+path('about-us', views.about_view, name="about-us"),
+path('contact-us', views.contact_view, name="contact-us")
 ]
--- web/views.py
+++ web/views.py
@@ -2,5 +2,17 @@ from django.shortcuts import render
 
 
 def homepage_view(request):
-	return render(request, 'web/homepage.html')
+	return render(request, 'web/index.html')
+
+def projects_view(request):
+	return render(request, 'web/Our-Projects.html')
+
+def learn_view(request):
+	return render(request, 'web/Learn.html')
+
+def about_view(request):
+	return render(request, 'web/About.html')
+
+def contact_view(request):
+	return render(request, 'web/Contact.html')
 # Create your views here.
