83ce75028fa70896883b1bd4db5f39db36055c6a
Thuhaa <josephthuha@gmail.com>
2021-08-07 17:01:25 +0300

    Configured CRUD for the App

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Project, Course
+from django.contrib.auth.models import Group
 
-# Register your models here.
+class CourseAdmin(admin.ModelAdmin):
+	list_display = ('title', 'category', 'date_created', 'link')
+class ProjectAdmin(admin.ModelAdmin):
+	list_display = ('title', 'date_created', 'link')
+
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.unregister(Group)
--- web/models.py
+++ web/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+COURSE_CATEGORIES = (("Video", "Video"), ("Blog", "Blog"))
+class Course(models.Model):
+	title = models.CharField(max_length=100)
+	image = models.ImageField(upload_to="course-images/")
+	category = models.CharField(choices=COURSE_CATEGORIES, max_length=10)
+	date_created = models.DateField(auto_now_add=True)
+	link = models.CharField(max_length=200)
+
+	class Meta:
+		verbose_name_plural = "Courses"
+
+	def __str__(self):
+		return self.title
+
+
+class Project(models.Model):
+	title = models.CharField(max_length=100)
+	image = models.ImageField(upload_to="course-images/")
+	summary = models.TextField(max_length=200)
+	date_created = models.DateField(auto_now_add=True)
+	link = models.CharField(max_length=200)
+
+	class Meta:
+		verbose_name_plural = "Projects"
+
+	def __str__(self):
+		return self.title
