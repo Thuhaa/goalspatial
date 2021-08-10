8f8a75f4ab411ff5f2d9011a9c06b290e13d8834
Thuhaa <josephthuha@gmail.com>
2021-08-10 20:54:57 +0300

    Minor Change

--- goalspatial/settings.py
+++ goalspatial/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY_GOALSPATIALWEB')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['goalspatial.herokuapp.com', 'localhost']
 
