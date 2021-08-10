9ebd0b63db085749e4c4b57904d8dbb265b3da3d
Thuhaa <josephthuha@gmail.com>
2021-08-10 19:03:14 +0300

    Hosts Config

--- goalspatial/settings.py
+++ goalspatial/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY_GOALSPATIALWEB')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['goalspatial.herokuapp.com', 'localhost']
 
 
 # Application definition
