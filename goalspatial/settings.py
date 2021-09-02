acaeae9395b98fa115d57b5bbd7d1d311d80a73b
Thuhaa <josephthuha@gmail.com>
2021-09-02 13:06:58 +0300

    Err 1

--- goalspatial/settings.py
+++ goalspatial/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY_GOALSPATIALWEB')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['goalspatial.herokuapp.com','goalspatial.com','www.goalspatial.com','localhost']
 
