fd04ffb9053bdcfe14d04935f627f2ee37189836
Thuhaa <josephthuha@gmail.com>
2021-09-02 13:08:40 +0300

    Err 1-sol-1

--- demos/urls.py
+++ demos/urls.py
@@ -3,5 +3,5 @@ from .import views
 
 urlpatterns = [
 path('leaflet/getting-started-with-leaflet', views.leaflet_getting_started, name='leaflet-getting-started'),
-path('geography-game', views.geography_game, name='geography-game')
+path('geography-game', views.geography_game, name='geography-game'),
 path('leaflet/major-towns-population', views.population_view, name='population')]
