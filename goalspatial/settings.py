4e2a033c08a967d4099fb35c281f161b3e1cbc27
Thuhaa <josephthuha@gmail.com>
2021-10-09 17:09:15 +0300

    Minor Changes

--- goalspatial/settings.py
+++ goalspatial/settings.py
@@ -127,4 +127,4 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
-#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
